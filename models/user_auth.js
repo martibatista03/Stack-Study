a1e6b42fc4a033a52baf532a415f30f4bcb7e09d
Martí Batista Obiols <71564709+martibatista03@users.noreply.github.com>
2021-02-06 00:28:15 +0100

    Update user_auth.js

--- models/user_auth.js
+++ models/user_auth.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userAuthentication = new Schema({
-    isAuth: { type: String, required: true },
-    username: { type: String, required: true }
+    username: { type: String, required: true },
+    userId: { type: String, required: true }
 });
 
 module.exports = mongoose.model('userAuthentication', userAuthentication);
