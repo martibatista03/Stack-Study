12397b41e1d7cd59a0584496754bc64fce925eae
Martí Batista Obiols <71564709+martibatista03@users.noreply.github.com>
2021-02-06 00:27:47 +0100

    Update register.js

--- models/register.js
+++ models/register.js
@@ -4,7 +4,12 @@ const Schema = mongoose.Schema;
 const registerSchema = new Schema({
     username: { type: String, required: true },
     mail: { type: String, required: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    studyScope: { type: String, required: false },
+    speciality: { type: String, required: false },
+    description: { type: String, required: false },
+    upvotes: { type: Number, default: 0, required: false },
+    upvotedAnswers: { type: Array, required: false }
 });
 
 module.exports = mongoose.model('registerForm', registerSchema);
